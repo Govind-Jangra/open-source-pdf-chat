fd81668d36de182300ab2ad1476afe435552c241
Govind Jangra <govind70821@gmail.com>
2024-07-02 14:00:34 +0530

    feat: add hono for api calls and add get embedding call

--- cloudflare-worker/src/index.ts
+++ cloudflare-worker/src/index.ts
@@ -1,6 +1,24 @@
-export interface Env {}
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+import { Ai } from "@cloudflare/ai";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.post("/healthcheck", async (c) => {
+    return c.json({ message: "worker is running" });
+});
+
+app.post("/embedding", async (c:any) => {
+  const { text } = await c.req.json();
+  const ai = new Ai(c.env.AI);
+  const EMBEDDING_MODEL = c.env.EMBEDDING_MODEL;
+  const embeddings:any = await ai.run(EMBEDDING_MODEL, { text });
+  const vectors= embeddings.data[0];
+  return c.json({ vectors });
+});
+
+app.onError((err:any, c) => {
+  console.log("err : ", err);
+  return c.text(err.message, err.status ? err.status: 500);
+});
+
+export default app;

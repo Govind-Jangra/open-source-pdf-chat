4e9f44f1641fb3ae706e513b746e84f6bacd341a
Govind Jangra <govind70821@gmail.com>
2024-07-03 16:43:38 +0530

    feat: added mongoose schema and setup mongodb connection

---
+++ nextjs/src/lib/db/models.ts
@@ -0,0 +1,15 @@
+'use server'
+import pdfChatModelFactory from "./pdfChat";
+import messageModelFactory from "./pdfChatMessage"; 
+import dbConnectionFactory from './db';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('Database URL not found');
+}
+
+const dbConnection = dbConnectionFactory(process.env.DATABASE_URL);
+
+const PDFChat = pdfChatModelFactory(dbConnection);
+const PDFChatMessage = messageModelFactory(dbConnection);
+
+export { PDFChat, PDFChatMessage };
